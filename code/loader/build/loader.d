fd2b796c13bea201d331ed519ed8fb6e28b10e5c
mariomadproductions <9919020+mariomadproductions@users.noreply.github.com>
2022-06-28 20:01:34 +0100

    Sorted out code

---
+++ code/loader/build/loader.d
@@ -0,0 +1,4 @@
+loader.o: \
+ /home/user/game/mod/nsmb2-asm/PatchTemplate/loader/source/loader.c \
+ /home/user/game/mod/nsmb2-asm/PatchTemplate/loader/source/newcodeinfo.h
+/home/user/game/mod/nsmb2-asm/PatchTemplate/loader/source/newcodeinfo.h:
